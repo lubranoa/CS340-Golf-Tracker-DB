7ffdafca5102eec8de217382a5e72d17dac9f160
Alexander Lubrano <lubranoa@flip3.engr.oregonstate.edu>
2022-07-17 17:31:44 -0700

    Connect database to app.py and add route

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import os
+import database.db_connector as db
 
 # Configuration
 
 app = Flask(__name__)
+db_connection = db.connect_to_database()
 
 # Routes
 
 @app.route('/')
 def root():
     return render_template("main.j2")
+
+@app.route('/players')
+def players():
+    query = "SELECT * FROM players;"
+
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    results = json.dumps(cursor.fetchall())
+
+    return results
 
 # Listener
 
