a69bc8a4a5d06c76d077f890ad7a566ac2504e95
Alexander Lubrano <lubranoa@flip3.engr.oregonstate.edu>
2022-07-18 16:48:49 -0700

    Add html to templates and connect to db and app.py

--- app.py
+++ app.py
@@ -20,12 +20,54 @@ def players():
     results = cursor.fetchall()
     return render_template("players.j2", gt_players=results)
 
+@app.route('/player_clubs')
+def player_clubs():
+    query = "SELECT * FROM clubs;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("player_clubs.j2", gt_player_clubs=results)
+
+@app.route('/clubs')
+def clubs():
+    query = "SELECT * FROM clubs ORDER BY brand DESC;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("clubs.j2", gt_clubs=results)
+
+@app.route('/player_rounds')
+def player_rounds():
+    query = "SELECT * FROM rounds;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("player_rounds.j2", gt_rounds=results)
+
+@app.route('/player_club_swings')
+def player_club_swings():
+    query = "SELECT * FROM swings;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("player_round_swings.j2", gt_player_round_swings=results)\
+
+@app.route('/player_round_swings')
+def player_round_swings():
+    query = "SELECT * FROM swings;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("player_club_swings.j2", gt_player_club_swings=results)
+
 @app.route('/courses')
 def courses():
     query = "SELECT * FROM courses;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("courses.j2", gt_courses=results)
+
+@app.route('/course_holes')
+def course_holes():
+    query = "SELECT * FROM holes;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("courses.j2", gt_holes=results)
 
 # Listener
 
