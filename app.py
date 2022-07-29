e6d869767c98a84bcd974d950712bb334cb08507
Alexander Lubrano <lubranoa@flip3.engr.oregonstate.edu>
2022-07-29 05:49:51 -0700

    Change port

--- app.py
+++ app.py
@@ -352,5 +352,5 @@ def delete_swing(id):
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 15433))
+    port = int(os.environ.get('PORT', 15432))
     app.run(port=port, debug=True)
