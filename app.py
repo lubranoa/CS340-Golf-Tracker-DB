5449ca6e823b714e67bc9bb5485025e31e444acd
Conner Marchell <marchelc@oregonstate.edu>
2022-08-05 12:33:34 -0700

    Implemented first pass of player_clubs_search

--- app.py
+++ app.py
@@ -68,6 +68,26 @@ def read_player_clubs():
     return render_template("read_player_clubs.j2", gt_player_clubs=results)
 
 
+@app.route('/player-clubs/search')
+def read_player_clubs_search():
+    """Route that handles displaying player_clubs intersection table"""
+
+    player_name = request.args.get("player_name")
+
+    db_connection = db.connect_to_database()
+    query = f"""SELECT player_clubs.player_id, players.player_name, player_clubs.club_id, CONCAT(clubs.brand, ' ', clubs.club_name, ' ', clubs.club_type) AS club
+                FROM player_clubs
+                INNER JOIN players
+                ON player_clubs.player_id = players.player_id
+                INNER JOIN clubs
+                ON player_clubs.club_id = clubs.club_id
+                WHERE players.player_name LIKE '%{player_name}%';"""
+
+    cursor = db.execute_query(db_connection=db_connection, query=query, query_params=None)
+    results = cursor.fetchall()
+    return render_template("read_player_clubs.j2", gt_player_clubs=results)
+
+
 @app.route('/players')
 def read_players():
     """Route that handles displaying players table"""
